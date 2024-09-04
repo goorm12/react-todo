86c28915a668c4b76084f2507f10c90a217bd815
binwoonki <vinwoonki98@gmail.com>
2024-09-04 21:24:35 +0900

    Editor 컴포넌트 UI 수정 form 태그 추가

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,8 +2,10 @@ import "./Editor.css";
 const Editor = () => {
   return (
     <div className="Editor">
-      <input type="text" placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <form>
+        <input type="text" placeholder="새로운 Todo..." />
+        <button>추가</button>
+      </form>
     </div>
   );
 };
