a8e2574deb709e6030cd859381cf5f0ecc0ae0fb
binwoonki <vinwoonki98@gmail.com>
2024-09-04 21:32:02 +0900

    MockData를 만들어 연습하기, 추가 버튼, 엔터키 입력 시 추가하는 기능 추가하기

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./Editor.css";
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onCreate(content);
+  };
   return (
     <div className="Editor">
-      <form>
-        <input type="text" placeholder="새로운 Todo..." />
+      <form onSubmit={onSubmit}>
+        <input
+          value={content}
+          onChange={onChangeContent}
+          type="text"
+          placeholder="새로운 Todo..."
+        />
         <button>추가</button>
       </form>
     </div>
