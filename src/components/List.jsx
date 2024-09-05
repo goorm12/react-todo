c0d2775cccbe23bf6516676c43b4c4ef7bdde673
binwoonki <vinwoonki98@gmail.com>
2024-09-05 14:24:24 +0900

    검색어를 입력하여 맞는 리스트를 출력하기

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input type="text" placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        type="text"
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos-wrapper">
-        {todos.map((todo) => {
+        {filteredTodos.map((todo) => {
           return <TodoItem key={todo.id} {...todo} />;
         })}
       </div>
